ec24b61e4ae4bec974da608ed07523bf55c1380a
eraser5th <83107074+eraser5th@users.noreply.github.com>
2024-07-10 20:24:02 +0900

    fix timer

--- src/timer.ts
+++ src/timer.ts
@@ -1,35 +1,43 @@
-import { Cell, CellLoop, Stream, StreamSink, Unit } from "sodiumjs";
+import {
+  Cell,
+  CellLoop,
+  Stream,
+  StreamSink,
+  Transaction,
+  Unit,
+} from "sodiumjs";
 
 export class Timer {
-  public c_seconds: Cell<number>;
-  public s_finished: Stream<Unit>;
+  public c_seconds: Cell<number> = new Cell(0);
+  public s_finished: Stream<Unit> = new Stream();
 
   constructor(s_add_seconds: Stream<number>) {
-    const cloop_seconds = new CellLoop<number>();
-    this.c_seconds = cloop_seconds;
-
     const ssink_oneSecondPassed = new StreamSink<Unit>();
 
-    cloop_seconds.loop(
-      s_add_seconds
-        .orElse(ssink_oneSecondPassed.map(() => -1))
-        .snapshot(this.c_seconds, (a, b) => a + b)
-        .filter((s) => s >= 0)
-        .hold(0),
-    );
+    Transaction.run(() => {
+      const cloop_seconds = new CellLoop<number>();
+      this.c_seconds = cloop_seconds;
+      const cloop_working = new CellLoop<boolean>();
+      const s_seconds_is_zero = ssink_oneSecondPassed.gate(
+        this.c_seconds.map((s) => s === 0),
+      );
+      this.s_finished = s_seconds_is_zero.gate(cloop_working);
 
-    const s_seconds_is_zero = ssink_oneSecondPassed.gate(
-      this.c_seconds.map((s) => s === 0),
-    );
+      cloop_seconds.loop(
+        s_add_seconds
+          .orElse(ssink_oneSecondPassed.map(() => -1))
+          .snapshot(this.c_seconds, (a, b) => a + b)
+          .filter((s) => s >= 0)
+          .hold(0),
+      );
 
-    const cloop_working = new CellLoop<boolean>();
-    this.s_finished = s_seconds_is_zero.gate(cloop_working);
-    cloop_working.loop(
-      this.s_finished
-        .map(() => false)
-        .orElse(s_add_seconds.map(() => true))
-        .hold(false),
-    );
+      cloop_working.loop(
+        this.s_finished
+          .map(() => false)
+          .orElse(s_add_seconds.map(() => true))
+          .hold(false),
+      );
+    });
 
     setInterval(() => ssink_oneSecondPassed.send(Unit.UNIT), 1000);
   }
