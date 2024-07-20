0edf4cf5e50bd37ee09159ddf0201f80350dc719
eraser5th <83107074+eraser5th@users.noreply.github.com>
2024-07-20 16:40:40 +0900

    move app.ts into app dir

---
+++ src/app/app.ts
@@ -0,0 +1,22 @@
+import { Box } from "@/components/box";
+import Button from "@/components/button";
+import { Meter } from "@/components/meter";
+import { ViewItem } from "@/components/viewItem";
+import { Timer } from "@/timer";
+
+export const app = (): ViewItem => {
+  const addMinuteButton = new Button("+1 Minute");
+  const s_add_minute = addMinuteButton.s_clicked.map(() => 60);
+
+  const timer = new Timer(s_add_minute);
+
+  timer.s_finished.listen(() => {
+    alert("timer is finished!");
+  });
+
+  const buttons = new Box().appendChildren(addMinuteButton);
+  const meter = new Meter(timer.c_seconds);
+  const root = new Box().appendChildren(meter, buttons);
+
+  return root;
+};
