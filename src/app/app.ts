cd3330bfeb99ac56989e4bd3975bc343f32fb6a5
eraser5th <83107074+eraser5th@users.noreply.github.com>
2024-07-20 20:09:15 +0900

    update app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,18 +2,19 @@ import Button from "@/components/button";
 import { ViewItem } from "@/components/viewItem";
 import { simulator } from "./simulator";
 import { pot } from "./pot/pot";
-import { Cell, CellLoop, Stream, Unit } from "sodiumjs";
+import { CellLoop, Stream, Unit } from "sodiumjs";
 
 export const app = (): ViewItem => {
   const s_tick = new Stream<Unit>();
 
-  const c_waterIn = new Cell(0);
+  const waterInButton = new Button("水追加");
+
   const cloop_heaterPower = new CellLoop<number>();
   const cloop_hotWaterSupply = new CellLoop<boolean>();
 
   const simulator_out = simulator({
     s_tick,
-    c_waterIn: c_waterIn,
+    c_waterIn: waterInButton.c_pushing.map(() => 100),
     c_heaterPower: cloop_heaterPower,
     c_hotWaterSupply: cloop_hotWaterSupply,
   });
@@ -23,17 +24,17 @@ export const app = (): ViewItem => {
   const warmingConfigButton = new Button("保温設定");
   const lockButton = new Button("解除");
   const cover = new Button("ふた");
+  const hotWaterSupplyButton = new Button("給湯");
 
   const pot_out = pot({
     s_tick,
-    s_overflowSensor: simulator_out.s_waterOverflowSensor,
-    s_temperatureSensor: simulator_out.s_temperatureSensor,
-    s_waterLevelSensor: simulator_out.s_waterLevelSensor,
+    ...simulator_out,
     s_voilButtonClicked: voilButton.s_clicked,
     s_timerButtonClicked: timerButton.s_clicked,
-    s_warmingButtonClicked: warmingConfigButton.s_clicked,
+    s_warmingConfigButtonClicked: warmingConfigButton.s_clicked,
     s_lockButtonClicked: lockButton.s_clicked,
     s_cover: cover.s_clicked,
+    c_hotWarterSupplyButtonPushing: hotWaterSupplyButton.c_pushing,
   });
 
   cloop_heaterPower.loop(pot_out.c_heaterPower);
