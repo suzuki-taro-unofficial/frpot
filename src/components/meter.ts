52a57ff6d3c0a0349e44f83b4de595297fb5776e
eraser5th <83107074+eraser5th@users.noreply.github.com>
2024-07-27 21:30:54 +0900

    fix meter

--- src/components/meter.ts
+++ src/components/meter.ts
@@ -7,97 +7,18 @@ const meterStyle = css`
   flex-direction: column;
   gap: 4px;
 `;
-const meterLevelStyle = css`
+
+const meterLevelBaseStyle = css`
   display: flex;
   align-items: end;
   height: 50px;
   width: 20px;
-  background-color: #999999;
+  background: gray;
   border: solid 1px #444444;
 `;
 
-const meterLevelInnserStyle = css`
-  height: attr(data-percentage %);
-  width: 100%;
-  background-color: #449944;
-`;
-
-injectGlobal`
-[data-percentage="1"] {
-  height: 4%;
-}
-[data-percentage="2"] {
-  height: 8%;
-}
-[data-percentage="3"] {
-  height: 12%;
-}
-[data-percentage="4"] {
-  height: 16%;
-}
-[data-percentage="5"] {
-  height: 20%;
-}
-[data-percentage="6"] {
-  height: 24%;
-}
-[data-percentage="7"] {
-  height: 28%;
-}
-[data-percentage="8"] {
-  height: 32%;
-}
-[data-percentage="9"] {
-  height: 36%;
-}
-[data-percentage="10"] {
-  height: 40%;
-}
-[data-percentage="11"] {
-  height: 44%;
-}
-[data-percentage="12"] {
-  height: 48%;
-}
-[data-percentage="13"] {
-  height: 52%;
-}
-[data-percentage="14"] {
-  height: 56%;
-}
-[data-percentage="15"] {
-  height: 60%;
-}
-[data-percentage="16"] {
-  height: 64%;
-}
-[data-percentage="17"] {
-  height: 68%;
-}
-[data-percentage="18"] {
-  height: 72%;
-}
-[data-percentage="19"] {
-  height: 76%;
-}
-[data-percentage="20"] {
-  height: 80%;
-}
-[data-percentage="21"] {
-  height: 84%;
-}
-[data-percentage="22"] {
-  height: 88%;
-}
-[data-percentage="23"] {
-  height: 92%;
-}
-[data-percentage="24"] {
-  height: 96%;
-}
-[data-percentage="25"] {
-  height: 100%;
-}
+const meterLevelLitStyle = css`
+  background: rightblue;
 `;
 
 export class Meter implements ViewItem<HTMLDivElement> {
@@ -106,10 +27,6 @@ export class Meter implements ViewItem<HTMLDivElement> {
   private meterLevel2: HTMLDivElement;
   private meterLevel3: HTMLDivElement;
   private meterLevel4: HTMLDivElement;
-  private meterLevel1Inner: HTMLDivElement;
-  private meterLevel2Inner: HTMLDivElement;
-  private meterLevel3Inner: HTMLDivElement;
-  private meterLevel4Inner: HTMLDivElement;
 
   constructor(c_percentage: Cell<0 | 1 | 2 | 3 | 4>) {
     this.meter = document.createElement("div");
@@ -118,18 +35,10 @@ export class Meter implements ViewItem<HTMLDivElement> {
     this.meterLevel2 = document.createElement("div");
     this.meterLevel3 = document.createElement("div");
     this.meterLevel4 = document.createElement("div");
-    this.meterLevel1.className = meterLevelStyle;
-    this.meterLevel2.className = meterLevelStyle;
-    this.meterLevel3.className = meterLevelStyle;
-    this.meterLevel4.className = meterLevelStyle;
-    this.meterLevel1Inner = document.createElement("div");
-    this.meterLevel2Inner = document.createElement("div");
-    this.meterLevel3Inner = document.createElement("div");
-    this.meterLevel4Inner = document.createElement("div");
-    this.meterLevel1Inner.className = meterLevelInnserStyle;
-    this.meterLevel2Inner.className = meterLevelInnserStyle;
-    this.meterLevel3Inner.className = meterLevelInnserStyle;
-    this.meterLevel4Inner.className = meterLevelInnserStyle;
+    this.meterLevel1.classList.add(meterLevelBaseStyle);
+    this.meterLevel2.classList.add(meterLevelBaseStyle);
+    this.meterLevel3.classList.add(meterLevelBaseStyle);
+    this.meterLevel4.classList.add(meterLevelBaseStyle);
 
     this.meter.append(
       this.meterLevel4,
@@ -137,10 +46,6 @@ export class Meter implements ViewItem<HTMLDivElement> {
       this.meterLevel2,
       this.meterLevel1,
     );
-    this.meterLevel1.append(this.meterLevel1Inner);
-    this.meterLevel2.append(this.meterLevel2Inner);
-    this.meterLevel3.append(this.meterLevel3Inner);
-    this.meterLevel4.append(this.meterLevel4Inner);
 
     Transaction.run(() => {
       Operational.value(c_percentage).listen((percentage) => {
@@ -150,37 +55,32 @@ export class Meter implements ViewItem<HTMLDivElement> {
     });
   }
 
-  private changeMeter(percentage: number) {
-    if (percentage < 0) {
-      this.meterLevel1Inner.dataset.percentage = `${0}`;
-      this.meterLevel2Inner.dataset.percentage = `${0}`;
-      this.meterLevel3Inner.dataset.percentage = `${0}`;
-      this.meterLevel4Inner.dataset.percentage = `${0}`;
-    } else if (percentage < 25 * 1) {
-      this.meterLevel1Inner.dataset.percentage = `${percentage - 25 * 0}`;
-      this.meterLevel2Inner.dataset.percentage = `${0}`;
-      this.meterLevel3Inner.dataset.percentage = `${0}`;
-      this.meterLevel4Inner.dataset.percentage = `${0}`;
-    } else if (percentage < 25 * 2) {
-      this.meterLevel1Inner.dataset.percentage = `${25}`;
-      this.meterLevel2Inner.dataset.percentage = `${percentage - 25 * 1}`;
-      this.meterLevel3Inner.dataset.percentage = `${0}`;
-      this.meterLevel4Inner.dataset.percentage = `${0}`;
-    } else if (percentage < 25 * 3) {
-      this.meterLevel1Inner.dataset.percentage = `${25}`;
-      this.meterLevel2Inner.dataset.percentage = `${25}`;
-      this.meterLevel3Inner.dataset.percentage = `${percentage - 25 * 2}`;
-      this.meterLevel4Inner.dataset.percentage = `${0}`;
-    } else if (percentage < 25 * 4) {
-      this.meterLevel1Inner.dataset.percentage = `${25}`;
-      this.meterLevel2Inner.dataset.percentage = `${25}`;
-      this.meterLevel3Inner.dataset.percentage = `${25}`;
-      this.meterLevel4Inner.dataset.percentage = `${percentage - 25 * 3}`;
-    } else {
-      this.meterLevel1Inner.dataset.percentage = `${25}`;
-      this.meterLevel2Inner.dataset.percentage = `${25}`;
-      this.meterLevel3Inner.dataset.percentage = `${25}`;
-      this.meterLevel4Inner.dataset.percentage = `${25}`;
+  private changeMeter(level: 0 | 1 | 2 | 3 | 4) {
+    if (level === 0) {
+      this.meterLevel1.classList.add(meterLevelLitStyle);
+      this.meterLevel2.classList.remove(meterLevelLitStyle);
+      this.meterLevel3.classList.remove(meterLevelLitStyle);
+      this.meterLevel4.classList.remove(meterLevelLitStyle);
+    } else if (level === 1) {
+      this.meterLevel1.classList.add(meterLevelLitStyle);
+      this.meterLevel2.classList.add(meterLevelLitStyle);
+      this.meterLevel3.classList.remove(meterLevelLitStyle);
+      this.meterLevel4.classList.remove(meterLevelLitStyle);
+    } else if (level === 2) {
+      this.meterLevel1.classList.add(meterLevelLitStyle);
+      this.meterLevel2.classList.add(meterLevelLitStyle);
+      this.meterLevel3.classList.remove(meterLevelLitStyle);
+      this.meterLevel4.classList.remove(meterLevelLitStyle);
+    } else if (level === 3) {
+      this.meterLevel1.classList.add(meterLevelLitStyle);
+      this.meterLevel2.classList.add(meterLevelLitStyle);
+      this.meterLevel3.classList.add(meterLevelLitStyle);
+      this.meterLevel4.classList.remove(meterLevelLitStyle);
+    } else if (level === 4) {
+      this.meterLevel1.classList.add(meterLevelLitStyle);
+      this.meterLevel2.classList.add(meterLevelLitStyle);
+      this.meterLevel3.classList.add(meterLevelLitStyle);
+      this.meterLevel4.classList.add(meterLevelLitStyle);
     }
   }
 
