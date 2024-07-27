1967eb65b0040178a3d376f113eac109e7ccb1c0
eraser5th <83107074+eraser5th@users.noreply.github.com>
2024-07-27 19:54:53 +0900

    coreのerror周りを切り出し

--- src/app/pot/core.ts
+++ src/app/pot/core.ts
@@ -31,7 +31,7 @@ type Output = {
   c_lock: Cell<boolean>;
 };
 
-export const core = ({}: Input): Output => {
+export const core = ({ }: Input): Output => {
   return {
     // for simulator
     c_heaterPower: new Cell(0),
@@ -44,99 +44,6 @@ export const core = ({}: Input): Output => {
     c_timer: new Cell(0),
     c_lock: new Cell(true),
   };
-};
-
-type TargetTemperatureInput = {
-  c_status: Cell<Status>;
-  c_warmLevel: Cell<KeepWarmMode>;
-};
-
-export const target_temperature = ({
-  c_status,
-  c_warmLevel,
-}: TargetTemperatureInput): Cell<number> => {
-  return c_status.lift(c_warmLevel, (status, warmLevel): number => {
-    switch (status) {
-      case "Boil":
-        return 100;
-      case "KeepWarm":
-        switch (warmLevel) {
-          case "High":
-            return 98;
-          case "Economy":
-            return 90;
-          case "Milk":
-            return 60;
-        }
-      case "Stop":
-        return 0; // TODO: 適切な温度は？
-    }
-  });
-};
-
-type ErrorTemperatureNotIncreasedInput = {
-  s_tick: Stream<number>;
-  s_temperature: Stream<number>;
-  c_status: Cell<Status>;
-  c_warmLevel: Cell<KeepWarmMode>;
-};
-
-// FIXME: 壊れた実装かもしれない
-export const error_temperature_not_increased = ({
-  s_tick,
-  s_temperature,
-  c_status,
-  c_warmLevel,
-}: ErrorTemperatureNotIncreasedInput): Stream<Unit> => {
-  return Transaction.run(() => {
-    const c_prevTime = new CellLoop<number>();
-    const s_oneMinutesPassed = s_tick
-      .snapshot(c_prevTime, (currTime, prevTime) => {
-        if (currTime - prevTime >= 60 * 1000) {
-          return currTime;
-        } else {
-          return null;
-        }
-      })
-      .filterNotNull() as Stream<number>;
-    c_prevTime.loop(s_oneMinutesPassed.hold(Date.now()));
-
-    const c_targetTemp = target_temperature({ c_status, c_warmLevel });
-    const c_currTemp = s_temperature.hold(0);
-    const c_prevTemp = s_oneMinutesPassed
-      .snapshot(c_currTemp, (_, temp) => temp)
-      .hold(0);
-
-    return s_tick
-      .snapshot4(
-        c_currTemp,
-        c_prevTemp,
-        c_targetTemp,
-        (_, currTemp, prevTemp, targetTemp) => {
-          return currTemp - 5 <= targetTemp && prevTemp > currTemp;
-        },
-      )
-      .filter((cond) => {
-        return cond;
-      })
-      .mapTo(new Unit());
-  });
-};
-
-type ErrorTemperatureTooHighInput = {
-  s_temperature: Stream<number>;
-};
-
-export const error_temperature_too_hight = ({
-  s_temperature,
-}: ErrorTemperatureTooHighInput): Stream<Unit> => {
-  return s_temperature
-    .filter((temp) => {
-      return temp > 110;
-    })
-    .map((_) => {
-      return new Unit();
-    });
 };
 
 type StatusInput = {
