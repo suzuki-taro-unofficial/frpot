a27c9b60976facdd627025ee434455184200dc25
eraser5th <83107074+eraser5th@users.noreply.github.com>
2024-07-27 15:33:06 +0900

    presenterのシグネチャを作成[wip]

--- src/app/pot/presenter.ts
+++ src/app/pot/presenter.ts
@@ -1,7 +1,36 @@
-type Input = {};
+import { Cell } from "sodiumjs";
 
-type Output = {};
+type Input = {
+  // TODO: 型を決める
+  c_mode: Cell<any>;
+  c_warmLevel: Cell<any>;
+  c_temperature: Cell<number>;
+  c_waterLevel: Cell<0 | 1 | 2 | 3 | 4>;
+};
 
-export const presenter = ({ }: Input): Output => {
-  return {};
+type Output = {
+  c_isLitVoilingModeLamp: Cell<boolean>;
+  c_isLitWarmingModeLamp: Cell<boolean>;
+  c_isLitWarmHighLamp: Cell<boolean>;
+  c_isLitWarmSavingsLamp: Cell<boolean>;
+  c_isLitWarmMilkLamp: Cell<boolean>;
+  c_temperatureLCD: Cell<string>;
+  c_waterLevelMeter: Cell<0 | 1 | 2 | 3 | 4>;
+  c_timerLCD: Cell<string>;
+};
+
+export const presenter = ({
+  c_mode,
+  c_warmLevel,
+  c_temperature,
+  c_waterLevel,
+}: Input): Output => {
+  return {
+    c_isLitVoilingModeLamp: c_mode.map((mode) => mode === "VOILING"),
+    c_isLitWarmingModeLamp: c_mode.map((mode) => mode === "WARMING"),
+    c_isLitWarmHighLamp: c_warmLevel.map((level) => level === "HIGH"),
+    c_isLitWarmSavingsLamp: c_warmLevel.map((level) => level === "SAVING"),
+    c_isLitWarmMilkLamp: c_warmLevel.map((level) => level === "MILK"),
+    c_waterLevelMeter: c_warmLevel,
+  };
 };
