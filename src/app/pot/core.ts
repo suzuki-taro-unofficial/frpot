5a07987f7bf4d696bd92aeacbd9aa8d443298a5f
pogyomo <pogyomo@gmail.com>
2024-07-27 16:13:37 +0900

    fix: error_temperature_not_increased

--- src/app/pot/core.ts
+++ src/app/pot/core.ts
@@ -68,7 +68,7 @@ export const target_temperature = ({
 };
 
 type ErrorTemperatureNotIncreasedInput = {
-  s_tick: Stream<Unit>;
+  s_tick: Stream<number>;
   s_temperature: Stream<number>;
   c_mode: Cell<Mode>;
   c_warmLevel: Cell<KeepWarmMode>;
@@ -81,13 +81,33 @@ export const error_temperature_not_increased = ({
   c_mode,
   c_warmLevel,
 }: ErrorTemperatureNotIncreasedInput): Stream<Unit> => {
-  const c_targetTemp = target_temperature({ c_mode, c_warmLevel });
-  const c_prevTemp = s_temperature.hold(0);
-
-  return s_temperature
-    .snapshot3(c_prevTemp, c_targetTemp, (currTemp, prevTemp, targetTemp) => {
-      return currTemp - 5 <= targetTemp && prevTemp > currTemp;
+  const c_prevTime = new CellLoop<number>();
+  const s_oneMinutesPassed = s_tick
+    .snapshot(c_prevTime, (currTime, prevTime) => {
+      if (currTime - prevTime >= 60 * 1000) {
+        return currTime;
+      } else {
+        return null;
+      }
     })
+    .filterNotNull() as Stream<number>;
+  c_prevTime.loop(s_oneMinutesPassed.hold(Date.now()));
+
+  const c_targetTemp = target_temperature({ c_mode, c_warmLevel });
+  const c_currTemp = s_temperature.hold(0);
+  const c_prevTemp = s_oneMinutesPassed
+    .snapshot(c_currTemp, (_, temp) => temp)
+    .hold(0);
+
+  return s_tick
+    .snapshot4(
+      c_currTemp,
+      c_prevTemp,
+      c_targetTemp,
+      (_, currTemp, prevTemp, targetTemp) => {
+        return currTemp - 5 <= targetTemp && prevTemp > currTemp;
+      },
+    )
     .filter((cond) => {
       return cond;
     })
