5024ea73eb35db28a093c78f2fc695141894bd80
pogyomo <pogyomo@gmail.com>
2024-08-28 15:57:33 +0900

    build: update pdf build system

--- md-to-pdf.sh
+++ md-to-pdf.sh
@@ -1,7 +1,7 @@
 set -e
 
 pandoc -d md-to-html.yaml
-pandoc -d html-to-pdf.yaml
+pandoc -d html-to-pdf.yaml --metadata date="`date -u "+%Y-%m-%d"`"
 rm output.html
-mv output.pdf revs/SLAMFRPゼミ資料.pdf
+mv output.pdf revs/frpotゼミ資料.pdf
 echo "✅ Generate succeded."
