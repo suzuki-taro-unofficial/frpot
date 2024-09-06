7937099de7e9e8a6393f57360ae42de03da52a5c
soukouki <soukouki0@yahoo.co.jp>
2024-09-06 13:23:04 +0900

    fix typo

--- src/app/pot/core/timer.ts
+++ src/app/pot/core/timer.ts
@@ -15,17 +15,17 @@ export const timer = (inputs: TimerInput): TimerOutput => {
   return Transaction.run(() => {
     const s_add = inputs.s_timerButtonClicked.mapTo(Time.minute_to_ms(1));
     const c_remainigTime = new CellLoop<number>();
-    const s_accumalatedTime = inputs.s_tick
+    const s_accumulatedTime = inputs.s_tick
       .map((s_tick) => -s_tick)
       .merge(s_add, (elapsedTime, add) => add + elapsedTime)
       .snapshot(c_remainigTime, (delta, remaining) => remaining + delta);
 
-    const s_newTime = s_accumalatedTime.map(
+    const s_newTime = s_accumulatedTime.map(
       (hoge) => Math.max(0, hoge) % Time.hour_to_ms(1),
     );
     c_remainigTime.loop(s_newTime.hold(0));
 
-    const s_beep = s_accumalatedTime
+    const s_beep = s_accumulatedTime
       .filter((hoge) => hoge === 0)
       .mapTo(new Unit());
 
